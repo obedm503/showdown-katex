a514fa8681ed67cdcba03448eb99201e24109add
obedm503 <obedm503@gmail.com>
2019-09-06 12:55:05 -0500

    - better build process - deploy docs to gh-pages

---
+++ docs.js
@@ -0,0 +1,16 @@
+// @ts-check
+const { join } = require('path');
+const { writeFile, readFile, ensureDir } = require('fs-extra');
+const ejs = require('ejs');
+
+(async () => {
+  const input = await readFile(join(__dirname, 'index.ejs'), 'utf-8');
+  const SRC =
+    process.env.NODE_ENV === 'production'
+      ? 'https://unpkg.com/showdown-katex@latest/dist/showdown-katex.min.js'
+      : 'showdown-katex.js';
+
+  const out = await ejs.render(input, { SRC }, { async: true });
+  await ensureDir(join(__dirname, 'dist'));
+  await writeFile(join(__dirname, 'dist/index.html'), out);
+})();
