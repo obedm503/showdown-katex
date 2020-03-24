a37754d7654734cf457fa07a8b69b7fc2c55628b
obedm503 <obedm503@gmail.com>
2020-03-24 14:31:58 -0500

    code-style math to uses "lang" sub-extensions

--- src/showdown-katex.js
+++ src/showdown-katex.js
@@ -1,3 +1,4 @@
+// @ts-check
 import katex from 'katex';
 import renderMathInElement from 'katex/dist/contrib/auto-render';
 import showdown from 'showdown';
@@ -6,112 +7,101 @@ import asciimathToTex from './asciimath-to-tex';
 if (process.env.TARGET === 'cjs') {
   const { JSDOM } = require('jsdom');
   const jsdom = new JSDOM();
+  // @ts-ignore
   global.DOMParser = jsdom.window.DOMParser;
+  // @ts-ignore
   global.document = jsdom.window.document;
 }
 
 /**
- * @param {object} opts
- * @param {NodeListOf<Element>} opts.elements
- * @param opts.config
- * @param {boolean} opts.isAsciimath
+ * @param {string} latex
+ * @param config
  */
-function renderBlockElements({ elements, config, isAsciimath }) {
-  if (!elements.length) {
-    return;
-  }
-
-  elements.forEach(element => {
-    const input = element.textContent;
-    const latex = isAsciimath ? asciimathToTex(input) : input;
-    const html = katex.renderToString(latex, config);
-    element.parentNode.outerHTML = `<span title="${input.trim()}">${html}</span>`;
-  });
-}
-
-/**
- * https://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
- * @param {string} str
- * @returns {string} regexp escaped string
- */
-function escapeRegExp(str) {
-  return str.replace(/[-[\]/{}()*+?.\\$^|]/g, '\\$&');
+function renderBlock(latex, config) {
+  const html = katex.renderToString(latex, config);
+  return `<div title="${latex.trim()}">${html}</div>`;
 }
 
 // katex config
-const getConfig = (config = {}) => ({
-  displayMode: true,
-  throwOnError: false, // fail silently
-  errorColor: '#ff0000',
-  ...config,
-  delimiters: (config.delimiters || []).concat([
+function getConfig(config = {}) {
+  const delimiters = (config.delimiters || []).concat([
     { left: '$$', right: '$$', display: false },
     { left: '~', right: '~', display: false, asciimath: true },
-  ]),
-});
+  ]);
+  return Object.assign(
+    {
+      displayMode: true,
+      throwOnError: false, // fail silently
+      errorColor: '#ff0000',
+    },
+    config,
+    { delimiters },
+  );
+}
 
-const showdownKatex = userConfig => () => {
+export default function showdownKatex(userConfig) {
   const parser = new DOMParser();
   const config = getConfig(userConfig);
-  const asciimathDelimiters = config.delimiters
-    .filter(item => item.asciimath)
-    .map(({ left, right }) => {
-      const test = new RegExp(
-        `${escapeRegExp(left)}(.*?)${escapeRegExp(right)}`,
-        'g',
-      );
-      const replacer = (match, asciimath) => {
-        return `${left}${asciimathToTex(asciimath)}${right}`;
-      };
-      return { test, replacer };
-    });
 
-  return [
-    {
-      type: 'output',
-      filter(html = '') {
-        // parse html
-        const wrapper = parser.parseFromString(html, 'text/html').body;
+  const asmDelimiters = [];
+  const texDelimiters = [];
+  config.delimiters.forEach(d => {
+    if (d.asciimath) {
+      asmDelimiters.push(d);
+    } else {
+      texDelimiters.push(d);
+    }
+  });
+  const asmConfig = Object.assign({}, config, {
+    delimiters: asmDelimiters,
+    preProcess: asciimathToTex,
+  });
+  const texConfig = Object.assign({}, config, { delimiters: texDelimiters });
 
-        if (asciimathDelimiters.length) {
-          // convert inline asciimath to inline latex
-          // ignore anything in code and pre elements
-          wrapper.querySelectorAll(':not(code):not(pre)').forEach(el => {
-            /** @type Text[] */
-            const textNodes = [...el.childNodes].filter(
-              // skip "empty" text nodes
-              node => node.nodeName === '#text' && node.nodeValue.trim(),
-            );
-
-            textNodes.forEach(node => {
-              const newText = asciimathDelimiters.reduce(
-                (acc, { test, replacer }) => acc.replace(test, replacer),
-                node.nodeValue,
-              );
-              node.nodeValue = newText;
-            });
-          });
-        }
-
-        // find the math in code blocks
-        const latex = wrapper.querySelectorAll('code.latex.language-latex');
-        const asciimath = wrapper.querySelectorAll(
-          'code.asciimath.language-asciimath',
-        );
-
-        renderBlockElements({ elements: latex, config });
-        renderBlockElements({ elements: asciimath, config, isAsciimath: true });
-
-        renderMathInElement(wrapper, config);
-
-        // return html without the wrapper
-        return wrapper.innerHTML;
+  return () => {
+    return [
+      // render block-style math
+      {
+        type: 'lang',
+        regex: /(?:^```asciimath)([\s\S]*?)(?:^```)/gm,
+        replace(block, asm) {
+          const latex = asciimathToTex(asm);
+          return renderBlock(latex, config);
+        },
       },
-    },
-  ];
-};
+      {
+        type: 'lang',
+        regex: /(?:^```latex)([\s\S]*?)(?:^```)/gm,
+        replace(block, latex) {
+          return renderBlock(latex, config);
+        },
+      },
+      // render inline math
+      {
+        type: 'output',
+        filter(html = '') {
+          if (!config.delimiters.length) {
+            return html;
+          }
+
+          // parse html
+          const wrapper = parser.parseFromString(html, 'text/html').body;
+
+          if (asmDelimiters.length) {
+            renderMathInElement(wrapper, asmConfig);
+          }
+
+          if (texDelimiters.length) {
+            renderMathInElement(wrapper, texConfig);
+          }
+
+          // return html without the wrapper
+          return wrapper.innerHTML;
+        },
+      },
+    ];
+  };
+}
 
 // register extension with default config
 showdown.extension('showdown-katex', showdownKatex());
-
-export default showdownKatex;
